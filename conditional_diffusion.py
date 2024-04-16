d949cf83d1bd81fa9dcfe9b252fa7958d2e9249d
ryoryoe <ryo.lutchi@gmail.con>
2024-04-17 08:42:14 +0900

    add preprocess over border_vy

--- conditional_diffusion.py
+++ conditional_diffusion.py
@@ -277,14 +277,14 @@ class HyperParameters:
     #ファイル関連
     task_name: str = "estimate_velocity"
     #output_path: str = "diffusion_model_0221_T=20_from5_to20"
-    output_path: str = "diffusion_model_0416_T=20" #出力先のフォルダ名
+    output_path: str = "diffusion_model_0416_T=5_ver11_over_vy_0.4" #出力先のフォルダ名
     #output_path: str = "diffusion_model_0408_sum_and_cat" #出力先のフォルダ名
     file_path: str = "train_data_ver6_test" #推定に使うデータのフォルダ
-    train_file_path = "train_data_ver7" #学習データのフォルダ
-    train_path: str = f"../{train_file_path}/Time=20" #学習データ
-    train_eval_path: str  = f"../{train_file_path}/Time=20" #学習データの正解ラベル
-    test_path: str = f"../{file_path}/Time=20" #推定に使うデータ
-    test_eval_path: str  = f"../{file_path}/Time=20" #推定に使うデータ(意味ない)
+    train_file_path = "train_data_ver11" #学習データのフォルダ
+    train_path: str = f"../{train_file_path}/Time=5" #学習データ
+    train_eval_path: str  = f"../{train_file_path}/Time=5" #学習データの正解ラベル
+    test_path: str = f"../{file_path}/Time=5" #推定に使うデータ
+    test_eval_path: str  = f"../{file_path}/Time=5" #推定に使うデータ(意味ない)
     weight_eval_path = f"../result/{output_path}/weight_{output_path}.pth" #学習済みモデルの名前
     
     #ハイパーパラメーター
@@ -292,9 +292,9 @@ class HyperParameters:
     save_interval: int = 10 #何エポックごとにモデルを保存するか
     learning = 1 #1で学習を行う,0で学習を行わずに推定のみを行う
     standard = 0 #1で標準化を行う,0で行わない
-    epochs: int = 50 #エポック数
+    epochs: int = 200 #エポック数
     width: int = 32 #画像の幅
-    batch_size: int = 256 #バッチサイズ
+    batch_size: int = 128 #バッチサイズ
     lr: float = 1.0e-3 #学習率
     time_steps: int =  1000  # T もう少し小さくても良いはず,何回ノイズを加えるか
     image_ch: int = 2 #画像のチャンネル数(xとyの速度の2つ)
