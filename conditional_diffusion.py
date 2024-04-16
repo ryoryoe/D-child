2cb984ddad8c7b8ac8c252e06a76fb09f64c67a0
ryoryoe <ryo.lutchi@gmail.con>
2024-04-16 22:24:40 +0900

    restart  only diffusion model

--- conditional_diffusion.py
+++ conditional_diffusion.py
@@ -277,20 +277,20 @@ class HyperParameters:
     #ファイル関連
     task_name: str = "estimate_velocity"
     #output_path: str = "diffusion_model_0221_T=20_from5_to20"
-    output_path: str = "diffusion_model_0408_sum" #出力先のフォルダ名
+    output_path: str = "diffusion_model_0416_T=20" #出力先のフォルダ名
     #output_path: str = "diffusion_model_0408_sum_and_cat" #出力先のフォルダ名
     file_path: str = "train_data_ver6_test" #推定に使うデータのフォルダ
     train_file_path = "train_data_ver7" #学習データのフォルダ
-    train_path: str = f"../{train_file_path}/Time=5" #学習データ
+    train_path: str = f"../{train_file_path}/Time=20" #学習データ
     train_eval_path: str  = f"../{train_file_path}/Time=20" #学習データの正解ラベル
-    test_path: str = f"../{file_path}/Time=5" #推定に使うデータ
+    test_path: str = f"../{file_path}/Time=20" #推定に使うデータ
     test_eval_path: str  = f"../{file_path}/Time=20" #推定に使うデータ(意味ない)
     weight_eval_path = f"../result/{output_path}/weight_{output_path}.pth" #学習済みモデルの名前
     
     #ハイパーパラメーター
     cut_size: int = 300000 #訓練データのサイズ(実際には10%はテストデータとして使う。全て使う時は大きい数を指定)
     save_interval: int = 10 #何エポックごとにモデルを保存するか
-    learning = 0 #1で学習を行う,0で学習を行わずに推定のみを行う
+    learning = 1 #1で学習を行う,0で学習を行わずに推定のみを行う
     standard = 0 #1で標準化を行う,0で行わない
     epochs: int = 50 #エポック数
     width: int = 32 #画像の幅
